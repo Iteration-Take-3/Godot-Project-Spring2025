3dd2a0f7eb1e1de0ccbad5d37117d56da13b8138
CocoCrawlt <c.byars349@montytech.net>
2025-05-09 12:57:27 -0400

    WIP ledgegrab mechanic

--- spring-game-project-2025/ledge.gd
+++ spring-game-project-2025/ledge.gd
@@ -1,6 +1,9 @@
-extends CollisionShape2D
+extends Area2D
 
+var x = position.x
+var y = position.y
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.parent.is_in_group("player"):
-		area.parent.position.x = position.x;
+	if area.get_parent().is_in_group("player"):
+		area.get_parent().position.x = x;
+		area.get_parent().position.y = y + 50
