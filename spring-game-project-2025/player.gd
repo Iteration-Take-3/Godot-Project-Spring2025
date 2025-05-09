276969b73cdf6c6ac6dd154a9105cf5d3a46cde5
CocoCrawlt <c.byars349@montytech.net>
2025-05-09 12:09:29 -0400

    Faster fastfalling

--- spring-game-project-2025/player.gd
+++ spring-game-project-2025/player.gd
@@ -39,7 +39,7 @@ func _physics_process(delta: float) -> void:
 		get_tree().quit()
 		
 	if Input.is_action_just_pressed("Down") && not is_on_floor():
-		velocity.y = 500
+		velocity.y = 750;
 		
 	move_and_slide()
 
