c93197ded828a148cdc9c9da779353a0739a9d80
CocoCrawlt <c.byars349@montytech.net>
2025-05-09 11:20:47 -0400

    Random Music

--- spring-game-project-2025/main_stage.gd
+++ spring-game-project-2025/main_stage.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
 func _ready():
-	$AudioStreamPlayer2D.play();
+	var rng = RandomNumberGenerator.new();
+	var thing = rng.randf_range(0.00, 10.00);
+	print(thing);
+	if thing >= 5:
+		$AltTrack.play();
+	else:
+		$MainTrack.play();
