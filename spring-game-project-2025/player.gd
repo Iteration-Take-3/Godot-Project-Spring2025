5fffbb7895a95286c2ba153049db873329525648
CocoCrawlt <c.byars349@montytech.net>
2025-05-09 11:32:38 -0400

    You can fastfall now

--- spring-game-project-2025/player.gd
+++ spring-game-project-2025/player.gd
@@ -37,7 +37,10 @@ func _physics_process(delta: float) -> void:
 	
 	if stocks < 1:
 		get_tree().quit()
-	
+		
+	if Input.is_action_just_pressed("Down") && not is_on_floor():
+		velocity.y = 500
+		
 	move_and_slide()
 
 
