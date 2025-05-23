80a4731472e504563abf9e3bda9ef77ee578579d
CocoCrawlt <c.byars349@montytech.net>
2025-05-23 13:48:15 -0400

    Lotsa assets. Getting ready to code damage and whatnot

---
+++ spring-game-project-2025/objects/scripts/fighters/aidism.gd
@@ -0,0 +1,15 @@
+extends "res://objects/scripts/fighters/dism.gd"
+
+func _physics_process(delta: float) -> void:
+	if stocks < 1:
+		get_tree().change_scene_to_file("res://scenes/debug_stages.tscn");
+	
+	if atk_timer > 0:
+		attack_handler();
+	
+	
+	velocity.x = 0
+	if (velocity.y < gravity_cap):
+		velocity.y += gravity * delta
+		
+	move_and_slide()
