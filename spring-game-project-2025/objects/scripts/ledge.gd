66187c3143ebd88492e2c6ffac060b2baa298065
Iteration-Take-3 <d.hawkins449@montytech.net>
2025-05-23 09:12:26 -0400

    Ledge Fix
    Fixed ledge a bit. Still a little buggy...

--- spring-game-project-2025/objects/scripts/ledge.gd
+++ spring-game-project-2025/objects/scripts/ledge.gd
@@ -2,11 +2,21 @@ extends Area2D
 
 var x = position.x
 var y = position.y
+var ledge = false
 
 func _on_area_entered(area: Area2D) -> void:
+	print(ledge)
 	if area.get_parent().is_in_group("player"):
-		area.get_parent().gravity = 0;
-		area.get_parent().velocity.x = 0;
-		area.get_parent().velocity.y = 0;
-		area.get_parent().jumpset();
-		area.get_parent().onledge = true;
+		ledge = true
+		if (!area.get_parent().is_on_floor() && ledge):
+			area.get_parent().gravity = 0;
+			area.get_parent().velocity.x = 0;
+			area.get_parent().velocity.y = 0;
+			area.get_parent().jumpset();
+			area.get_parent().onledge = true;
+			await Input.is_anything_pressed()
+			
+
+
+func _on_area_exited(area: Area2D) -> void:
+	ledge = false
