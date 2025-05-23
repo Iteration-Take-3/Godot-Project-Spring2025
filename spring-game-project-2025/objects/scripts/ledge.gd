946086edc19fe7090769097f0ad176e8548e5bca
Iteration-Take-3 <d.hawkins449@montytech.net>
2025-05-23 09:37:38 -0400

    Ledge fix
    Yes! Ledge fixed (for now)

--- spring-game-project-2025/objects/scripts/ledge.gd
+++ spring-game-project-2025/objects/scripts/ledge.gd
@@ -4,6 +4,7 @@ var x = position.x
 var y = position.y
 var ledge = false
 
+<<<<<<< Updated upstream
 
 func _on_body_entered(body: Node2D) -> void:
 	print(body)
@@ -15,9 +16,25 @@ func _on_body_entered(body: Node2D) -> void:
 			body.velocity.y = 0;
 			body.jumpset();
 			body.onledge = true;
+=======
+func _on_area_entered(area: Area2D) -> void:
+	print(ledge)
+	if area.get_parent().is_in_group("player"):
+		ledge = true
+		if (!area.get_parent().is_on_floor() && ledge):
+			area.get_parent().gravity = 0;
+			area.get_parent().velocity.x = 0;
+			area.get_parent().velocity.y = 0;
+			area.get_parent().jumpset();
+			area.get_parent().onledge = true;
+>>>>>>> Stashed changes
 			await Input.is_anything_pressed()
 			
 
 
+<<<<<<< Updated upstream
 func _on_body_exited(body: Node2D) -> void:
+=======
+func _on_area_exited(area: Area2D) -> void:
+>>>>>>> Stashed changes
 	ledge = false
